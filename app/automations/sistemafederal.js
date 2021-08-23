c3ff390ea14200e85b86c0b9ed29f213a61c47eb
Felipe Almeida <almeida.dy.felipe@gmail.com>
2021-08-23 15:23:24 -0300

    colocando um adicional na certidão

--- app/automations/sistemafederal.js
+++ app/automations/sistemafederal.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 async function startSistemaFederal() {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: ['--no-sandbox'],
     });
     const page = await browser.newPage();
